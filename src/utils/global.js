31665943ff84a8b47ed7ae640018d6353b5ee39d
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2025-01-13 21:47:46 +0700

    menambahkan Estimation Reading Time

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,3 +1,6 @@
+import getReadingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
+
 export const getFormattedDate = (date) =>
   date
     ? new Date(date).toLocaleDateString("en-us", {
@@ -6,3 +9,12 @@ export const getFormattedDate = (date) =>
         day: "numeric",
       })
     : "";
+
+// https://docs.astro.build/en/recipes/reading-time/
+export function remarkReadingTime() {
+  return function (tree, { data }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    data.astro.frontmatter.minutesRead = readingTime.text;
+  };
+}
